e65ad79a614b8e15fb80901d7f5db5c4444e3609
Vincent Raveneau <vincentraveneau@orange.fr>
2014-02-17 18:21:20 +0100

    Lecture du fichier de configuration

--- GestionnairePlugins/src/pluginManager/PluginManager.java
+++ GestionnairePlugins/src/pluginManager/PluginManager.java
@@ -1,5 +1,8 @@
 package pluginManager;
 
+import java.io.FileReader;
+import java.util.Properties;
+
 import interfaces.IPluginManager;
 
 public class PluginManager implements IPluginManager {
@@ -8,6 +11,36 @@ public class PluginManager implements IPluginManager {
 		// Lecture du fichier de conf indiquant les plugins à lancer
 			// lancement des plugins "simples"
 			// lancement des plugins "compliqués" (référence au PluginManager nécessaire)
+		readConfigFile("resources/init");
 	}
-
+	
+	// Lit le fichier donné et le traite
+	private static void readConfigFile(String filename) {
+		// Chargement du fichier
+		Properties prop = new Properties();
+		try {
+			prop.load(new FileReader(filename));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		// Traitement des données chargées
+		String[] pluginsToLoad = prop.getProperty("loadAtStart").split(",\\s*");
+		
+		if(pluginsToLoad.length > 0) {
+			for(String s : pluginsToLoad) {
+				String[] argsForPlugin = prop.getProperty(s).split(",\\s*");
+				
+				loadPlugin(s,argsForPlugin);
+			}
+		}
+	}
+	
+	// Charge un plugin donné selon les arguments donnés
+	private static void loadPlugin(String pluginName, String[] args) {
+		System.out.println(pluginName);
+		for(String s: args) {
+			System.out.println("arg: "+s);
+		}
+	}
 }
