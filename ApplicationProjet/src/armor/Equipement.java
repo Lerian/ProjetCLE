f0baef28b3c2eb5af88b93f38e85c9abbdd52d43
QuentinMoriceau <quentin.moriceau@gmail.com>
2014-02-27 17:26:51 +0100

    Ajout d'un enum pour la position des équipements

--- ApplicationProjet/src/armor/Equipement.java
+++ ApplicationProjet/src/armor/Equipement.java
@@ -3,4 +3,23 @@ package armor;
 public class Equipement {
 	String name;
 	Energy energyNeeded;
+	Position pos;
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Energy getEnergyNeeded() {
+        return energyNeeded;
+    }
+    public void setEnergyNeeded(Energy energyNeeded) {
+        this.energyNeeded = energyNeeded;
+    }
+    public Position getPos() {
+        return pos;
+    }
+    public void setPos(Position pos) {
+        this.pos = pos;
+    }
 }
---
+++ ApplicationProjet/src/armor/Position.java
@@ -0,0 +1,12 @@
+package armor;
+
+public enum Position {
+    HEAD,
+    BODY,
+    RHAND,
+    LHAND,
+    RLEG,
+    LLEG,
+    RARM,
+    LARM
+}
