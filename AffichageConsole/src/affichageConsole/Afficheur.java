940044b03e8a52f497156ba1b4777393df1ffc7e
Slayerxoxo <coraline.marie.cm@gmail.com>
2014-02-27 16:29:56 +0100

    initialisation plugin affichage console

---
+++ AffichageConsole/src/affichageConsole/Afficheur.java
@@ -0,0 +1,11 @@
+package affichageConsole;
+
+import interfaces.IPlugin;
+
+public class Afficheur implements IPlugin {
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+	}
+}
