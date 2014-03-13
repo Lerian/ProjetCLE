bd0bb585d7b48896d22e9e7373283589850874ef
Slayerxoxo <coraline.marie.cm@gmail.com>
2014-03-13 14:57:53 +0100

    Plugin Modificateur de données

---
+++ ApplicationProjet/src/interfaces/IModificateur.java
@@ -0,0 +1,29 @@
+package interfaces;
+
+import armor.Armor;
+import armor.Body;
+import armor.Energy;
+import armor.Equipement;
+import armor.Position;
+import armor.Weapon;
+
+public interface IModificateur {
+	//Modification de l'armure (Armor)
+	public void modifieNomArmure(Armor armure, String nomArmure);
+	
+	//Modification de l'énergie (Energy)
+	public void modifieNomEnergie(Energy energie, String nomEnergie);
+	public void modifieValEnergie(Energy energie, int valeurEnergie);
+	
+	//Modification de l'équipement (Equipement, Body, Weapon)
+	public void modifieNomEquipement(Equipement equipement, String nomEquipement);
+	public void modifieEnergieEquipement(Equipement equipement, Energy energie);
+	public void mofifiePosEquipement(Equipement equipement, Position position);
+	
+	//Modification de l'équipement du corps (Body)
+	public void modifieColorBody(Body body, String couleur);
+	public void modifieProtectBody(Body body, int protection);
+	
+	//Modification de l'équipement arme (Weapon)
+	public void modifieDamageWeapon(Weapon weapon, int damage);
+}
---
+++ ModificationArmure/src/modificationArmure/Modificateur.java
@@ -0,0 +1,74 @@
+package modificationArmure;
+
+import armor.Armor;
+import armor.Body;
+import armor.Energy;
+import armor.Equipement;
+import armor.Position;
+import armor.Weapon;
+import interfaces.IModificateur;
+import interfaces.IPlugin;
+
+public class Modificateur implements IPlugin, IModificateur{
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public void modifieNomArmure(Armor armure, String nomArmure) {
+		
+		armure.setName(nomArmure);
+	}
+
+	@Override
+	public void modifieNomEnergie(Energy energie, String nomEnergie) {
+		
+		energie.setName(nomEnergie);
+	}
+
+	@Override
+	public void modifieValEnergie(Energy energie, int valeurEnergie) {
+		
+		energie.setValue(valeurEnergie);
+	}
+
+	@Override
+	public void modifieNomEquipement(Equipement equipement, String nomEquipement) {
+		
+		equipement.setName(nomEquipement);
+	}
+
+	@Override
+	public void modifieEnergieEquipement(Equipement equipement, Energy energie) {
+
+		equipement.setEnergyNeeded(energie);
+	}
+
+	@Override
+	public void mofifiePosEquipement(Equipement equipement, Position position) {
+		
+		equipement.setPos(position);
+	}
+
+	@Override
+	public void modifieColorBody(Body body, String couleur) {
+		
+		body.setColor(couleur);
+	}
+
+	@Override
+	public void modifieProtectBody(Body body, int protection) {
+		
+		body.setProtection(protection);
+	}
+
+	@Override
+	public void modifieDamageWeapon(Weapon weapon, int damage) {
+		
+		weapon.setDamage(damage);
+	}
+	
+}
